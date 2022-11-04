88eb0b4bf47dbf05c6479dd701d5c400179ed504
NilanshuRanjan <116020416+NilanshuRanjan@users.noreply.github.com>
2022-11-04 21:10:46 +0530

    Add files via upload

---
+++ Snake_water_gun.py
@@ -0,0 +1,48 @@
+import random
+
+
+def snake_water_gun():
+    l = ["s","g","w"]
+    user_points = 0
+    comp_points = 0
+    n = int(input("\nenter max points: "))
+    while True:
+        user = input("\nenter s for snake w for water and g for gun: ")
+        comp_input = random.choice(l)
+
+        if comp_input == "s":
+            print("computer input is Snake")
+        if comp_input == "w":
+            print("computer input is Water")
+        if comp_input == "g":
+            print("computer input is Gun")
+
+        if user == "s":
+            if comp_input == "w":
+                user_points += 1
+            elif comp_input == "g":
+                comp_points += 1
+        elif user == "w":
+            if comp_input == "g":
+                user_points += 1
+            elif comp_input == "s":
+                comp_points += 1
+        elif user == "g":
+            if comp_input == "s":
+                user_points += 1
+            elif comp_input == "w":
+                comp_points += 1
+
+        print("computer score ==", comp_points, "\nYour score == ", user_points)
+
+        if comp_points == n:
+            print("computer wins")
+            break
+        elif user_points == n:
+            print("computer wins")
+            break
+    ch = input("\nwanna play again if yes then enter Y: ")
+    if ch in "Yy":
+        snake_water_gun()
+
+snake_water_gun()
